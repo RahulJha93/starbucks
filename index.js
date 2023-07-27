3df509305b88ff898cf69f2996d832ad21de1f74
unknown <rahul.sahyogcollege@gmail.com>
2023-07-27 19:43:05 +0530

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+
+
+
+
+
+
